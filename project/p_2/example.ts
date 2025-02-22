ad5ee534b844dc0cf18f0395f17217a55352df2d
Gustavo Rocha <gurocha05@gmail.com>
2025-02-22 15:23:41 -0300

    Create example.ts

---
+++ project/p_2/example.ts
@@ -0,0 +1,16 @@
+//      Variables example
+
+// simple sum in typescript using const as variable
+
+const sum = (v1: number, v2: number): number => {
+    return v1 + v2;
+}
+
+const total = sum(2, 2)
+
+// You cannot do
+//      const name = "Alex"
+//      name= "Gustavo"
+// use
+//      let name = "Alex"
+//      name= "Gustavo"
