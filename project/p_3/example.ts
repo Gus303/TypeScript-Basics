3bcab66224f056fc05e19cde2046c99056b27d45
Gustavo Rocha <gurocha05@gmail.com>
2025-02-22 15:37:31 -0300

    Update example.ts

--- project/p_3/example.ts
+++ project/p_3/example.ts
@@ -2,28 +2,28 @@
 
 // If / Else
 
-if(age >= 18 && creditCard) {
-    alert("User is over 18")
-} else (parentalPermission && creditCard) {
-    alert("User is under 18, with permission")
-} else {
-    alert("User is under 18, without a permission")
-}
+//if(age >= 18 && creditCard) {
+//    alert("User is over 18")
+//} else (parentalPermission && creditCard) {
+//    alert("User is under 18, with permission")
+//} else {
+//   alert("User is under 18, without a permission")
+//}
 
 // Switch
 
-switch(option) {
-    case 1:
-        // do something
-        break;
-    case 2:
-        // do something
-        break;
-    case 3:
-        // do something
-        break;
-}
+//switch(option) {
+//    case 1:
+//        // do something
+//        break;
+//    case 2:
+//        // do something
+//        break;
+//    case 3:
+//        // do something
+//        break;
+//}
 
 // Ternary Operator
 
-const prefix = user.gender === GeneratedIdentifierFlags.FEMALE ? "Mrs" : "Mr"
+//const prefix = user.gender === GeneratedIdentifierFlags.FEMALE ? "Mrs" : "Mr"
