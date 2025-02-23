bff28513eb38fdab0e8c5ca0e87167f53030f3e8
Gustavo Rocha <gurocha05@gmail.com>
2025-02-23 17:09:55 -0300

    Create generics.ts

---
+++ project/p_8/generics.ts
@@ -0,0 +1,29 @@
+// Generics:
+
+// Simple:
+// const names: Array<string> = ["Gustavo", "Alice"];
+
+// Custom:
+// const users: Array<User> = [
+//     { name: "Gustavo", age: 19 },
+//     { name: "Alice", age: 23 }
+// ];
+
+// Create one:
+// type UserPage = {
+//     list: Array<User>;
+//     totalPages: number;
+//     currentPages: number;
+//     itemsPerPage: number;
+//     totalItems: number;
+// }
+
+// OR
+
+// type Page<T> = {
+//     list: Array<T>;
+//     totalPages: number;
+//     currentPages: number;
+//     itemsPerPage: number;
+//     totalItems: number;
+// }
