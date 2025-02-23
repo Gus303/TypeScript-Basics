f83b94680360798d2701bebc0d503ce2e13b2844
Gustavo Rocha <gurocha05@gmail.com>
2025-02-23 16:20:23 -0300

    Create private.ts

---
+++ project/p_8/private.ts
@@ -0,0 +1,15 @@
+// Private:
+// class Dog {
+//     name: string = "";
+//     private energy: mumber = 100;
+
+//     constructor() {
+//         bark(): void {
+//             if (this.energy >= 30) {
+//                 console.log("Woof-woof");
+//                 this.energy -= 10;
+//             }
+//         }
+//     hear(noise: string): void {}
+//     }
+// }
