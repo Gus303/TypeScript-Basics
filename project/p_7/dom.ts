d751e48031ca39dd300ccf41b42d62cbe492188d
Gustavo Rocha <gurocha05@gmail.com>
2025-02-23 16:05:06 -0300

    Create dom.ts

---
+++ project/p_7/dom.ts
@@ -0,0 +1,12 @@
+// DOM:
+
+// ID:
+// const myElement: HTMLElement = document.getElementById("elementID")
+// document.getElementById(elementID: string) : HTMLElement
+
+// Div:
+// const myElement: HTMLDivElement = document.getElementById("asd") as HTMLDivElement
+
+// querySelector and createElement:
+// const elements: HTMLElement | null = document.querySelector("li")
+// const a: HTMLParagraphElement = document.createElement("p")
