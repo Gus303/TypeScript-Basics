e18aae6cdbab2ad9f3ce0756409481e6c7f98aa4
Gustavo Rocha <gurocha05@gmail.com>
2025-02-23 17:18:11 -0300

    dist

--- public/dist/Basics/p_1/example.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// type User = {
-//     firstName: string
-//     lastName: string
-// }
-// const fullName = (user: User): string => {
-//     return `${user.firstName} ${user.lastName}`
-// }
