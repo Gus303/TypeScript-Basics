b9f98186724087c67296417c87864554c5ba60d8
Gustavo Rocha <gurocha05@gmail.com>
2025-02-23 16:10:23 -0300

    Create class.ts

---
+++ project/p_8/class.ts
@@ -0,0 +1,24 @@
+// Class:
+// const dog: Dog = new Dog()
+
+// Constructor:
+// class Dog {
+//     name: string = ""
+//     weight: number = 0
+//     color: string = ""
+//     breed: string = ""
+//     size: number = ""
+
+//     constructor(
+//         name: string,
+//         weight: number,
+//         color: string,
+//         breed: string,
+//         size: number
+//     ) {
+//         this.name = name
+//         this.weight = weight
+//         this.color = color
+//         this.breed = breed
+//         this.size = size
+//     }
